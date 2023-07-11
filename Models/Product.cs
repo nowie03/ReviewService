ee1bee94034802945fb5915439121d98cf5222be
Immanuel <nowpertimmanuel@gmail.com>
2023-07-11 12:13:40 +0530

    fix : fixed product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,19 +3,21 @@
     public class Product
     {
 
+        
         public int Id { get; set; }
 
-
+     
         public int CategoryId { get; set; }
 
+     
+        public string Name { get; set; }
 
-
+     
         public double Price { get; set; }
-
 
         public string Description { get; set; }
 
-
+     
         public string Address { get; set; }
 
         public DateTime CreatedAt { get; set; }
